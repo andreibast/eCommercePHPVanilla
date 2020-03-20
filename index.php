1b01e597d25b2e2d7033d2f81ebc6ebf22a97158
Andrei <andrei.bast@yahoo.com>
2020-03-20 23:23:53 +0200

    part1done

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- adaptive meta for mobile phones -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>M-Dev Store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<div id="top"> <!-- Top Begin-->
+
+
+    <div class="container"> <!-- container Begin-->
+
+
+        <div class="col-md-6 offer"> <!-- col-md-6 offer1 Begin-->
+
+            <a href="" class="btn btn-sucess btn-sm">Welcome</a>
+            <a href="checkout.php">4 Items In Your Cart | Total Price: $300</a>
+
+        </div><!-- col-md-6 offer1 Finish-->
+
+        <div class="col-md-6"> <!-- col-md-6 offer2 Begin-->
+            <ul class="menu"><!-- menu Begin-->
+                <li>
+                    <a href="customer_register.php">Register</a>
+                </li>
+                <li>
+                    <a href="checkout.php">My Account</a>
+                </li>
+                <li>
+                    <a href="cart.php">Go To Cart</a>
+                </li>
+                <li>
+                    <a href="checkout.php">Login</a>
+                </li>
+
+            </ul> <!-- menu Finish-->
+
+        </div><!-- col-md-6 offer2 Finish-->
+
+
+
+    <div><!-- container Finish-->
+    
+    
+    
+    
+</div> <!-- Top Finish-->
+
+    <script src="js/bootstrap-337.min.js"></script>
+    <script src="js/jquery-331.min.js"></script>
+
+
+
+</body>
+</html>
