b71ae439b22bdafdedcd74601b1a8eb7cae71f6e
Andrei <andrei.bast@yahoo.com>
2020-04-13 14:20:46 +0300

    part35done

--- admin_area/index.php
+++ admin_area/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
     <div id="wrapper"><!-- #wrapper begin -->
        
        <?php include("includes/sidebar.php"); ?>
@@ -16,18 +17,21 @@
         <div id="page-wrapper"><!-- #page-wrapper begin -->
             <div class="container-fluid"><!-- container-fluid begin -->
                 
+                <?php
                 
+                    if(isset($_GET['dashboard'])){
+                        
+                        include("dashboard.php");
+                        
+                }
+                
+                ?>
                 
             </div><!-- container-fluid finish -->
         </div><!-- #page-wrapper finish -->
     </div><!-- wrapper finish -->
 
-
-
-    <script src="js/jquery-331.min.js"></script>
-    <script src="js/bootstrap-337.min.js"></script>
+<script src="js/jquery-331.min.js"></script>     
+<script src="js/bootstrap-337.min.js"></script>           
 </body>
 </html>
-
-
-
