9050286240b19a1eabb91e811330322e7813ef54
Andrei <andrei.bast@yahoo.com>
2020-04-27 22:37:27 +0300

    part51done

--- shop.php
+++ shop.php
@@ -7,8 +7,8 @@
    
    <div id="content"><!-- #content Begin -->
        <div class="container"><!-- container Begin -->
-
            <div class="col-md-12"><!-- col-md-12 Begin -->
+               
                <ul class="breadcrumb"><!-- breadcrumb Begin -->
                    <li>
                        <a href="index.php">Home</a>
@@ -17,57 +17,58 @@
                        Shop
                    </li>
                </ul><!-- breadcrumb Finish -->
+               
            </div><!-- col-md-12 Finish -->
            
-        <div class="col-md-3"><!-- col-md-3 Begin -->
+           <div class="col-md-3"><!-- col-md-3 Begin -->
    
-            <?php 
-                include("includes/sidebar.php");
-            ?>
+   <?php 
+    
+    include("includes/sidebar.php");
+    
+    ?> 
                
-        </div><!-- col-md-3 Finish -->
+           </div><!-- col-md-3 Finish -->
            
-        <div class="col-md-9"><!-- col-md-9 Begin -->
-             
-            
-                            <div class='box'><!-- box Begin -->
-                                <h1>Shop</h1>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo deleniti accusamus, consequuntur illum quasi ut. Voluptate a, ipsam repellendus ut fugiat minima? Id facilis itaque autem, officiis veritatis perferendis, quaerat!
-                                </p>
-                            </div><!-- box Finish -->
+           <div class="col-md-9"><!-- col-md-9 Begin -->
 
+                <div class='box'><!-- box Begin -->
+                    <h1>Shop</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo deleniti accusamus, consequuntur illum quasi ut. Voluptate a, ipsam repellendus ut fugiat minima? Id facilis itaque autem, officiis veritatis perferendis, quaerat!
+                    </p>
+                </div><!-- box Finish -->
                
                <div id="products" class="row"><!-- row Begin -->
-               
-               <?php getProducts(); ?>   
+
+               <?php getProducts(); ?>
                
                </div><!-- row Finish -->
                
                <center>
                    <ul class="pagination"><!-- pagination Begin -->
 
-                   <?php getPaginator(); ?> 
+                        <?php getPaginator(); ?>
 
                    </ul><!-- pagination Finish -->
                </center>
-                
-           
                
            </div><!-- col-md-9 Finish -->
 
            <div id="wait" style="position:absolute;top:40%;left:45%;padding: 200px 100px 100px 100px;"></div>
            
        </div><!-- container Finish -->
-    </div><!-- #content Finish -->
+   </div><!-- #content Finish -->
    
-    <?php 
-        include("includes/footer.php");
+   <?php 
+    
+    include("includes/footer.php");
+    
     ?>
     
-<script src="js/jquery-331.min.js"></script>
-<script src="js/bootstrap-337.min.js"></script>
-<script>
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    <script>
     
         $(document).ready(function(){
 
@@ -315,3 +316,7 @@
         });
     
     </script>
+    
+    
+</body>
+</html>
