0f5405a444f5da71a17c23563692bbf9e7074daa
Andrei <andrei.bast@yahoo.com>
2020-04-27 21:34:15 +0300

    part49done

--- admin_area/view_p_cats.php
+++ admin_area/view_p_cats.php
@@ -39,7 +39,7 @@
                             <tr><!-- tr begin -->
                                 <th> Product Category ID </th>
                                 <th> Product Category Title </th>
-                                <th> Product Category Desc </th>
+                                <th> Top Product Category </th>
                                 <th> Edit Product Category </th>
                                 <th> Delete Product Category </th>
                             </tr><!-- tr finish -->
@@ -50,14 +50,19 @@
                             <?php 
                             
                                 $i=0;
+          
                                 $get_p_cats = "select * from product_categories";
+          
                                 $run_p_cats = mysqli_query($con,$get_p_cats);
           
                                 while($row_p_cats=mysqli_fetch_array($run_p_cats)){
                                     
                                     $p_cat_id = $row_p_cats['p_cat_id'];
+                                    
                                     $p_cat_title = $row_p_cats['p_cat_title'];
-                                    $p_cat_desc = $row_p_cats['p_cat_desc'];
+                                    
+                                    $p_cat_top = $row_p_cats['p_cat_top'];
+                                    
                                     $i++;
                             
                             ?>
@@ -65,7 +70,7 @@
                             <tr><!-- tr begin -->
                                 <td> <?php echo $i; ?> </td>
                                 <td> <?php echo $p_cat_title; ?> </td>
-                                <td width="300"> <?php echo $p_cat_desc; ?> </td>
+                                <td width="300"> <?php echo $p_cat_top; ?> </td>
                                 <td> 
                                     <a href="index.php?edit_p_cat= <?php echo $p_cat_id; ?> ">
                                         <i class="fa fa-pencil"></i> Edit
